f7a098eb9242e4cf1e8f60806006f326a7b9f7eb
Bertrand Bichat <bertrand.bichat@gmail.com>
2020-03-04 13:35:17 +0100

    Yelp MVP - implementation of restaurants and reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,31 @@
+class ReviewsController < ApplicationController
+  def index
+    find_restaurant
+    @reviews = Review.where(restaurant_id: @restaurant.id)
+  end
+
+  def new
+    find_restaurant
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    find_restaurant
+    @review.restaurant = @restaurant
+    @review.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Nem shop',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number: '0456359613'
+  },
+  {
+    name:         'Pasta shop',
+    address:      '7 rue paradis, 13006 Marseille',
+    category:     'italian',
+    phone_number: '0434985612'
+  },
+  {
+    name:         'Sushi shop',
+    address:      '15 rue des marroniers, 75001 Paris',
+    category:     'japanese',
+    phone_number: '0652864017'
+  },
+  {
+    name:         'Burger shop',
+    address:      '3 rue Montgrand, 69008 Lyon',
+    category:     'french',
+    phone_number: '0956254814'
+  },
+  {
+    name:         'Frite shop',
+    address:      '27 rue tintin, Bruxelles',
+    category:     'belgian',
+    phone_number: '0742695613'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
